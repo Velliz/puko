cc0dcd326cb9d4226b35d8ca8c6a6f5c96112113
Didit Velliz <diditvelliz@outlook.com>
2016-03-23 16:45:52 +0700

    file upload example blob to db

--- Puko/Controllers/Example.php
+++ Puko/Controllers/Example.php
@@ -1,4 +1,5 @@
 <?php
+use Puko\Core\Data;
 
 /**
  * Class Example
@@ -18,5 +19,34 @@ class Example
             'PageTitle' => 'Puko Framework',
             'Welcome' => 'Welcome To Puko Framework Test Programs',
         );
+    }
+
+    function fileupload()
+    {
+
+        $dataSubmit = isset($_POST['_submit']);
+        if ($dataSubmit) {
+            //echo '<pre>';
+            //var_dump($_FILES);
+            //echo '</pre>';
+
+            Data::To('filex')->Save(
+                array(
+                    'name' => $_POST['name'],
+                    'email' => $_POST['email'],
+                    'file' => $_FILES['foto']['tmp_name']
+                )
+            );
+
+        }
+    }
+
+    function uno()
+    {
+    }
+
+    function duo()
+    {
+
     }
 }
