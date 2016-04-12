9700fd3da22043f160315ee9da652c07c6b8398c
Velliz <diditvelliz@gmail.com>
2016-04-12 10:21:26 +0700

    test case basic

--- Tests/Main.php
+++ Tests/Main.php
@@ -4,17 +4,24 @@ use Puko\Core\Puko;
 
 class Main extends PHPUnit_Framework_TestCase
 {
-    /**
-     * Main constructor.
-     */
-    public function __construct()
+    public function testPushAndPop()
     {
-        parent::__construct();
-    }
+        // Allocate a test fixture.
+        $fixture = array();
+        // Assert initially empty, count() is 0.
+        $this->assertEquals(0, count($fixture));
 
-    public function main()
-    {
-        \Puko\VariableDump(array('test' => 'root namespace'));
+        // Test array_push().
+        // Push an item into array.
+        array_push($fixture, 'foo');
+        // Assert one item, count() is 1.
+        $this->assertEquals(1, count($fixture));
+        // Assert item pushed.
+        $this->assertEquals('foo', $fixture[count($fixture) - 1]);
+
+        // Test array_pop().
+        $this->assertEquals('foo', array_pop($fixture));
+        $this->assertEquals(0, count($fixture));
     }
 
 }
