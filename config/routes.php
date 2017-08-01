be68918de76b2807c59a81680e571f068835fd5c
Velliz <diditvelliz@gmail.com>
2017-08-01 11:04:31 +0700

    scaffolds routes experimental

--- config/routes.php
+++ config/routes.php
@@ -1,28 +1,49 @@
-<?php
-
-$routes['page'] = array(
-    '' => [
-        'controller' => 'main',
-        'function' => 'main',
-        'accept' => ['GET', 'POST'],
+<?php $routes = [
+    "page" => [
+        "" => [
+            "controller" => "main",
+            "function" => "main",
+            "accept" => [
+                "GET",
+                "POST"
+            ]
+        ],
+        "another" => [
+            "controller" => "main",
+            "function" => "another",
+            "accept" => [
+                "GET",
+                "POST"
+            ]
+        ],
+        "multiple/folder" => [
+            "controller" => "folders\\user",
+            "function" => "folders",
+            "accept" => [
+                "GET"
+            ]
+        ],
+        "welcome/puko" => [
+            "controller" => "welcome",
+            "function" => "puko",
+            "accept" => [
+                "GET",
+                "POST"
+            ]
+        ]
     ],
-    'another' => [
-        'controller' => 'main',
-        'function' => 'another',
-        'accept' => ['GET', 'POST'],
+    "error" => [
+        "controller" => "",
+        "function" => "",
+        "accept" => [
+            "GET"
+        ]
+    ],
+    "not_found" => [
+        "controller" => "",
+        "function" => "",
+        "accept" => [
+            "GET"
+        ]
     ]
-);
-
-$routes['error'] = array(
-    'controller' => '',
-    'function' => '',
-    'accept' => ['GET'],
-);
-
-$routes['not_found'] = array(
-    'controller' => '',
-    'function' => '',
-    'accept' => ['GET'],
-);
-
-return $routes;
+]; return $routes;
