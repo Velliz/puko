a8f3f9b65cf893f77e20e787d6ea4bc3a9ce8729
Didit Velliz <diditvelliz@gmail.com>
2017-09-18 08:44:57 +0700

    update base uri

--- index.php
+++ index.php
@@ -9,7 +9,24 @@
 use pukoframework\Framework;
 
 require __DIR__ . '/vendor/autoload.php';
-define('BASE_URL', 'localhost');
+
+$protocol = 'http';
+if (isset($_SERVER['HTTPS'])) {
+    $protocol = 'https';
+} else if (isset($_SERVER['HTTP_X_SCHEME'])) {
+    $protocol = strtolower($_SERVER['HTTP_X_SCHEME']);
+} else if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+    $protocol = strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']);
+} else if (isset($_SERVER['SERVER_PORT'])) {
+    $serverPort = (int)$_SERVER['SERVER_PORT'];
+    if ($serverPort == 80) {
+        $protocol = 'http';
+    } else if ($serverPort == 443) {
+        $protocol = 'https';
+    }
+}
+
+define('BASE_URL', ($protocol . "://" . $_SERVER['HTTP_HOST'] . "/"));
 
 /*
  *---------------------------------------------------------------
