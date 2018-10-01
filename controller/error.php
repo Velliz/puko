0d2b341ef251a66f1740d4d49af0586b4f2d61c6
Didit Velliz <diditvelliz@gmail.com>
2018-10-01 08:20:51 +0700

    store default error controller

---
+++ controller/error.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace apps\front\controller;
+
+use pukoframework\middleware\View;
+
+/**
+ * Class error
+ * #Master master.html
+ */
+class error extends View
+{
+
+    /**
+     * #Value title Error Display
+     */
+    public function display()
+    {
+    }
+
+    /**
+     * #Value title Error Not Found (404)
+     */
+    public function notfound()
+    {
+    }
+
+}
