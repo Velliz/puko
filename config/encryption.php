d73f29f6044b1622b09b1a38edc7d23eb31f62f3
Didit Velliz <diditvelliz@gmail.com>
2019-04-22 16:01:55 +0700

    refactoring and cleaning some stuff

--- config/encryption.php
+++ config/encryption.php
@@ -1,7 +1,9 @@
 <?php return [
-    'method'     => 'AES-256-CBC',
-    'key'        => 'pukoframework',
+    'method' => 'AES-256-CBC',
+    'key' => 'pukoframework',
     'identifier' => 'p43mgf59nFR108dFD',
-    'cookies'    => 'puko',
-    'session'    => 'puko',
+    'cookies' => 'puko',
+    'session' => 'puko',
+    'expiredText' => "Login to continue",
+    'errorText' => "You don't have akses",
 ];
