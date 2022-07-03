095e6bf7e2d0ba9bb0d4a1c36a78332a24857ebd
Didit Velliz <diditvelliz@gmail.com>
2022-07-03 22:40:45 +0700

    database convention for ignored tables and hidden column in the database

--- config/database.php
+++ config/database.php
@@ -2,30 +2,42 @@
 
 $db['primary'] = [
     'dbType' => 'mysql',
-    'host'   => 'localhost',
-    'user'   => 'root',
-    'pass'   => '',
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
     'dbName' => '',
-    'port'   => '3306',
+    'port' => '3306',
     'driver' => '',
+    'ignoreTableWithPrefix' => '_',
+    'hideColumns' => [
+        'created', 'modified', 'cuid', 'muid', 'dflag', 'password'
+    ]
 ];
 $db['logs'] = [
     'dbType' => 'mysql',
-    'host'   => 'localhost',
-    'user'   => 'root',
-    'pass'   => '',
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
     'dbName' => '',
-    'port'   => '3306',
+    'port' => '3306',
     'driver' => '',
+    'ignoreTableWithPrefix' => '_',
+    'hideColumns' => [
+        'created', 'modified', 'cuid', 'muid', 'dflag', 'password'
+    ]
 ];
 $db['backup'] = [
     'dbType' => 'mysql',
-    'host'   => 'localhost',
-    'user'   => 'root',
-    'pass'   => '',
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => '',
     'dbName' => '',
-    'port'   => '3306',
+    'port' => '3306',
     'driver' => '',
+    'ignoreTableWithPrefix' => '_',
+    'hideColumns' => [
+        'created', 'modified', 'cuid', 'muid', 'dflag', 'password'
+    ]
 ];
 
 return $db;
